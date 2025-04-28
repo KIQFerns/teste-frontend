2552fcd42f13686ef132a66adb15ce512fc050fb
KIQFerns <52255280+KIQFerns@users.noreply.github.com>
2025-04-27 21:48:06 -0300

    feat: otimiza o navbar e a table component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 export default function NavBar({ onOpen }) {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-base-100 shadow-md">
       <div className="navbar-start">
         <a className="btn btn-ghost text-xl">Usuários</a>
       </div>
       <div className="navbar-center">
-        <h1>Teste fullstack</h1>
+        <h1 className="text-lg font-semibold">Teste Fullstack</h1>
       </div>
       <div className="navbar-end">
         <button onClick={onOpen} className="btn btn-primary">
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,60 +1,107 @@
+import { useState } from "react";
+import { useMessage } from "../context/MessageContext";
 import { deleteUser } from "../services/userService";
 
 export default function Table({ users, handleOpen, setUsers }) {
-  const handleDelete = async (id) => {
-    const confirmDelete = window.confirm(
-      "Você deseja realmente excluir esse usuário?"
-    );
-    if (confirmDelete) {
+  const { showError, showSuccess } = useMessage();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [userToDelete, setUserToDelete] = useState(null);
+
+  const openDeleteModal = (user) => {
+    setUserToDelete(user);
+    setIsModalOpen(true);
+  };
+
+  const closeDeleteModal = () => {
+    setIsModalOpen(false);
+    setUserToDelete(null);
+  };
+
+  const confirmDelete = async () => {
+    if (userToDelete) {
       try {
-        await deleteUser(id);
-        setUsers((prevData) => prevData.filter((client) => client.id !== id));
+        await deleteUser(userToDelete.id);
+        showSuccess("Usuário excluído com sucesso!");
+        setUsers((prevData) =>
+          prevData.filter((user) => user.id !== userToDelete.id)
+        );
       } catch (error) {
-        console.log(error);
+        showError("Erro ao excluir o usuário. Tente novamente!");
+        console.error(error);
       }
     }
+    closeDeleteModal();
   };
 
   return (
     <div className="overflow-x-auto mt-10">
-      <table className="table">
+      <table className="table table-zebra w-full">
         <thead>
           <tr>
-            <th></th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Status</th>
-            <th></th>
-            <th></th>
+            <th className="p-4">Nome</th>
+            <th className="p-4">Email</th>
+            <th className="p-4">Status</th>
+            <th className="p-4">Ações</th>
           </tr>
         </thead>
-        <tbody className="hover">
+        <tbody>
           {users.map((user) => (
             <tr key={user.id} className="hover">
-              <th></th>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-              <td>{user.active ? "Active" : "Inactive"}</td>
-              <td>
+              <td className="p-4">{user.name}</td>
+              <td className="p-4">{user.email}</td>
+              <td className="p-4 flex items-center">
+                <span
+                  className={`w-3 h-3 rounded-full mr-2 ${
+                    user.active ? "bg-green-500" : "bg-red-500"
+                  }`}
+                ></span>
+                {user.active ? "Ativo" : "Inativo"}
+              </td>
+              <td className="p-4">
                 <button
-                  className="btn btn-primary "
+                  className="btn btn-primary mr-2"
                   onClick={() => handleOpen("update", user)}
                 >
-                  Update
+                  Atualizar
                 </button>
-              </td>
-              <td>
                 <button
                   className="btn btn-secondary"
-                  onClick={() => handleDelete(user.id)}
+                  onClick={() => openDeleteModal(user)}
                 >
-                  Delete
+                  Excluir
                 </button>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
+
+      {isModalOpen && (
+        <div className="modal modal-open">
+          <div className="modal-box p-8 bg-white text-gray-900 rounded-lg shadow-lg">
+            <h2 className="text-lg font-semibold">
+              Tem certeza que deseja excluir?
+            </h2>
+            <p className="text-sm text-gray-600 mt-2">
+              Essa ação não pode ser desfeita.
+            </p>
+            <div className="flex gap-4 mt-6">
+              <button
+                className="btn btn-danger flex-1 py-2 px- font-semibold rounded-lg hover:bg-gray-400 transition duration-300"
+                onClick={confirmDelete}
+              >
+                Sim, excluir
+              </button>
+              <button
+                className="btn btn-ghost flex-1 py-2 px-4 text-gray-700 font-semibold rounded-lg hover:bg-gray-200 transition duration-300"
+                onClick={closeDeleteModal}
+              >
+                Cancelar
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
